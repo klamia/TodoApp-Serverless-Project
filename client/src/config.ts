a896f0a22e90c1e272d4f6b05af076f938eca2b3
klamia <klamia17@gmail.com>
2021-09-29 11:32:26 +0100

    Setting up backend and frontend for TodosApp

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '3kfmo5a9va'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-019ro45e.us.auth0.com',            // Auth0 domain
+  clientId: 'QquMm1JgxGz0gPPFNH1r3zixUop7aTFM',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
